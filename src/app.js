2b86e1cd1e57735264e318aabb1eec5abe61789c
Sweets <sweetmantech@gmail.com>
2020-05-29 15:51:00 -0400

    I add clock in functionality

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@ const { GoogleAssistant } = require('jovo-platform-googleassistant');
 const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
 
+//GOOGLE SPREADSHEET CONFIG
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+// spreadsheet key is the long id in the sheets URL
+const doc = new GoogleSpreadsheet('1_lXLitZ6mpTBT6tkRT7noBDq0yMePa40yHmscplXmkY');
+
 const app = new App();
 
 app.use(
@@ -25,17 +30,28 @@ app.use(
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        return this.toIntent('HelloWorldIntent');
+    async LAUNCH() {
+        // load directly from json file if not in secure environment
+        await doc.useServiceAccountAuth(require('./jovo-google-sheets-credentials.json'));
+        await doc.loadInfo(); // loads sheets
+        this.ask("Welcome to your timesheet assistant. Where are you working today?")
     },
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
-
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-    },
+    async CatchAllIntent() {
+        const userInput = this.$inputs.anything.value;
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1; //Returns the month (from 1-12)
+        const date = now.getDate();
+        const dayIndex = now.getDay();
+        const hour = now.getHours();
+        const minute = now.getMinutes() / 60; //Returns the minute (from 0-1)
+        const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        const dayOfWeek = daysOfWeek[dayIndex];
+        const clockInRow = await doc.sheetsByIndex[0].addRow({ Day: dayOfWeek, Date: month + "/" + date + "/" + year , Arrival: hour+minute, Location: userInput}); 
+        return this.tell('Hello World');
+    }
 });
+ 
 
 module.exports.app = app;
